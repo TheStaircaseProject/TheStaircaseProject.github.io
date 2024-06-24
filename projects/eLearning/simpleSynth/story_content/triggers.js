9feb4207fa558f6adbcd14cd12b2dd76dbd0e3ef
TheStaircaseProject <from.future.import.antigravity@gmail.com>
2024-06-24 11:04:02 -0400

    Uploaded Simple Synth

---
+++ projects/eLearning/simpleSynth/story_content/triggers.js
@@ -0,0 +1,51 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6ajTEICsNAh":
+        Script1();
+        break;
+      case "6TUbF2eed10":
+        Script2();
+        break;
+      case "6UT5kpMFpyv":
+        Script3();
+        break;
+      case "6N4Ub1Tpl88":
+        Script4();
+        break;
+      case "6jilQWpiUO8":
+        Script5();
+        break;
+      case "6QNjAmN9LXV":
+        Script6();
+        break;
+      case "6lTKYtOCmBN":
+        Script7();
+        break;
+      case "6NFykj0stez":
+        Script8();
+        break;
+      case "5ijOGTSL5FV":
+        Script9();
+        break;
+      case "6HqvTShEAky":
+        Script10();
+        break;
+      case "6XBPsgH30B8":
+        Script11();
+        break;
+      case "6XaDy1cyo3g":
+        Script12();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
